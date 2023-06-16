80785b76cf826453037ba0b217e22de42f3e786a
enduhab1 <abuhayendalew290@gmail.com>
2023-06-15 18:28:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 char a;
 int b;
-long int c
+long int c;
 long long int d;
 float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
@@ -17,3 +17,4 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
+
