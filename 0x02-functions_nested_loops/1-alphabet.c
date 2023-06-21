aa4d72f880339372f927c1d5d18fcc216c9567c8
enduhab1 <abuhayendalew290@gmail.com>
2023-06-21 08:07:36 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: Calls the print_alphabet function
+ * Return: Return(0)
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
