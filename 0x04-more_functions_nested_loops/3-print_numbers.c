1215fcf3a0fa9866a6a08d30d603a9beb6e80ea1
enduhab1 <abuhayendalew290@gmail.com>
2023-06-24 06:36:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - to print numbers from 0 to 9 by new line
+*Rturn: the number since 0 up to 9
+*Return none
+*/
+void print_numbers(void)
+
+{
+int j;
+for (j = 48; j <= 57; j++)
+{
+_putchar (j);
+}
+_putchar ('\n');
+
+}
