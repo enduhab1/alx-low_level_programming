0b9c5dc61a68e551042a667796986105d71f1071
enduhab1 <abuhayendalew290@gmail.com>
2023-06-24 10:17:14 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square in the terminal.
+ * @size: The size of the square.
+ *
+ * Description: Prints a square of '#' characters in the terminal.
+ */
+void print_square(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar('#');
+
+_putchar('\n');
+	}
+}
+
