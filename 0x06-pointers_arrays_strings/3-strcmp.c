ef180dfb23410a21087aa3a4c9c45f0828999d27
enduhab1 <abuhayendalew290@gmail.com>
2023-06-29 07:52:51 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strcmp -compare string value
+*@s1: inpute value
+*@s2: inpute value
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+i = 0;
+while (s1[i] != '\n' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
+}
