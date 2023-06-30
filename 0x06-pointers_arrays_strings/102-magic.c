1e69a5bb72f04f09eb0af4c0e957654cf8eeb9a2
enduhab1 <abuhayendalew290@gmail.com>
2023-06-30 13:27:47 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int a[] = {0, 1, 2, 3, 4};
+int *p = a + 2;
+
+printf("a[2] = %d\n", *(p));
+
+return (0);
+}
+
