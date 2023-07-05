94e24aca53adbfd73808fa8cf50f3c90cc3ada30
enduhab1 <abuhayendalew290@gmail.com>
2023-07-05 12:42:42 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -1,18 +1,27 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - check the code
+ * main - Entry point of the program.
+ *
+ * Description:
+ * This is the entry point of the program. It demonstrates the usage
+ * of the set_string function by setting the value of a pointer to a char.
+ * The original string is "Bob Dylan" and the new string is "Robert Allen".
+ * It prints the original and updated strings before and after the update.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *s0 = "Bob Dylan";
-    char *s1 = "Robert Allen";
+    char *name1 = "Bob Dylan";
+    char *name2 = "Robert Allen";
 
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
+    printf("%s, %s\n", name1, name2);
+
+    set_string(&name2, name1);
+
+    printf("%s, %s\n", name1, name2);
+
     return (0);
 }
+
